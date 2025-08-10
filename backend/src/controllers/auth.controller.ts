d46f7082dc32c0584cb43906e4668ada02b12c38
Vansh Kalra <vansh@erino.io>
2025-08-10 22:01:49 +0530

    Register API + Models completed

---
+++ backend/src/controllers/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import { asyncHandler } from "../middlewares/asyncHandler.middleware";
+import { HTTPSTATUS } from "../config/http.config";
+import { registerSchema } from "../validators/auth.validator";
+import { registerService } from "../services/auth.service";
+
+export const registerController = asyncHandler(
+  async (req: Request, res: Response) => {
+    const body = registerSchema.parse(req.body);
+
+    const result = await registerService(body);
+
+    res
+      .status(HTTPSTATUS.CREATED)
+      .json({ message: "User registered successfully", data: result });
+  }
+);
---
+++ backend/src/index.ts
@@ -0,0 +1,40 @@
+import "dotenv/config";
+import express, { NextFunction, Request, Response } from "express";
+import { Env } from "./config/env.config";
+import cors from "cors";
+import { HTTPSTATUS } from "./config/http.config";
+import { errorHandler } from "./middlewares/errorHandler.middleware";
+import { BadRequestException } from "./utils/app-error";
+import { asyncHandler } from "./middlewares/asyncHandler.middleware";
+import connectDatabase from "./config/database.config";
+import authRoutes from "./routes/auth.route";
+
+const app = express();
+const BASE_PATH = Env.BASE_PATH;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  cors({
+    origin: Env.FRONTEND_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.get(
+  "/",
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    throw new BadRequestException("Hmmmm");
+    res.status(HTTPSTATUS.OK).json({ message: "Hello Hire Me" });
+  })
+);
+
+app.use(`${BASE_PATH}/auth`, authRoutes);
+
+app.use(errorHandler);
+
+app.listen(Env.PORT, async () => {
+  await connectDatabase();
+  console.log(`Server is running on port ${Env.PORT} in ${Env.NODE_ENV} mode`);
+});
